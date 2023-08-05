d13bea11c0d9b838252621fa608668c025b2924f
KatyaT27 <turuntseva21@gmail.com>
2023-08-05 17:35:15 +0100

    Updated personal_helper.py with logging configuration and log messages.

--- personal_helper.py
+++ personal_helper.py
@@ -1,12 +1,30 @@
+import logging
 from ui.console_ui import ConsoleUI
 
 
-
 def main():
+    # Configure logging settings
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)s %(message)s',
+        level=logging.INFO,
+        filename='personal_helper.log',
+        filemode='a'
+    )
+
+    # Log the start of the Personal Helper Bot application
+    logging.info('Started Personal Helper Bot application.')
+
+    # Create an instance of the ConsoleUI class
     ui = ConsoleUI()
+
+    # Show the menu and interact with the user until the user chooses to exit
     while ui.show_menu():
         pass
 
+    # Log that the Personal Helper Bot application is exiting
+    logging.info('Exiting Personal Helper Bot application.')
+
 
 if __name__ == "__main__":
+
     main()
